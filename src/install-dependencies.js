076cf7d48fc154bb73af5187162331cb28b1229d
Vickjoe <55033116+vickjoeobi@users.noreply.github.com>
2023-03-22 08:25:22 +0100

    update the action

--- src/install-dependencies.js
+++
@@ -1,9 +0,0 @@
-const { execSync } = require('child_process');
-const path = require('path');
-
-const actionDirectory = path.join(__dirname, '..');
-const command = `npm ci --prefix "${actionDirectory}"`;
-
-console.log('Installing dependencies...');
-execSync(command, { stdio: 'inherit' });
-console.log('Dependencies installed.');
